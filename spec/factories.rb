3160fdcb4298c85b51135ae483ccedd494596171
Luke Seo <lukeseo@gmail.com>
2012-08-29 00:02:37 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+ factory :user do
+  name                  "Luke Seo"
+  email                 "lukeseo@gmail.com"
+  password              "foobar"
+  password_confirmation "foobar"
+  end
+end
