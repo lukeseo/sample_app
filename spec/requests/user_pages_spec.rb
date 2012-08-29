a839252999d0e7789262d27f63fa6993e397dc5d
Luke Seo <lukeseo@gmail.com>
2012-08-29 09:59:16 -0700

    Add more tests to User Signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,8 @@ describe "UserPages" do
     before { visit signup_path }
 
     it { should have_selector('h1',     text: 'Sign up') }
-    it { should have_selector('title',  text: full_title('Sign up'))}
+    it { should have_selector('title',  text: full_title('Sign up')) }
+    it { should_not have_content('Password digest') }
   end
 
   describe "profile page" do
@@ -30,6 +31,12 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content('error') }
+        end
     end    
   
     describe "with valid information" do
@@ -46,12 +53,11 @@ describe "UserPages" do
       end
 
       describe "after saving a user" do
-      
         before { click_button submit }
-
         let(:user) { User.find_by_email("user@example.com") }
 
         it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end    
   end
