f554e59a5943758d9e18b4dfeedbb330b6340a9a
Luke Seo <lukeseo@gmail.com>
2012-08-31 10:39:09 -0700

    Finish sign in and out 2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,11 +12,13 @@ describe "Authentication" do
 
       it { should have_selector('h1',    text: 'Sign in') }
       #it { should have_selector('title', text: 'Sign in') } 
-      it { should have_selector('div.alert.alert-error') }
+      #it { should have_selector('div.alert.alert-error') }
+      it { should have_error_message }
 
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
+        #it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_error_message }
       end
     end
 
